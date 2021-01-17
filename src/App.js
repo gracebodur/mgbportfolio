b11251b4b34cf69dccf95c8489529e82d2df9751
Grace <mgracebodur@gmail.com>
2021-01-16 21:08:38 -0500

    add css reset and create Intro component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import './App.css'
 import Topbar from './Topbar/Topbar'
+import Intro from './Intro/Intro'
 
 function App () {
 	return (
 		<div>
 			<Topbar />
+			<Intro />
 		</div>
 	)
 }
