fafac49bae79663a8686587710c2ec3d59a95b8f
Grace <mgracebodur@gmail.com>
2021-01-16 20:55:15 -0500

    add mailto attribute to create components files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './App.css'
-import Topbar from './Topbar'
+import Topbar from './Topbar/Topbar'
 
 function App () {
 	return (
--- src/Topbar/Topbar.js
+++ src/Topbar/Topbar.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import linkedin from './assets/linkedin.svg'
-import github from './assets/github.svg'
-import mail from './assets/email.svg'
-import docs from './assets//google-docs.svg'
+import linkedin from '../assets/linkedin.svg'
+import github from '../assets/github.svg'
+import mail from '../assets/email.svg'
+import docs from '../assets//google-docs.svg'
 import './Topbar.css'
 
 function Topbar () {
@@ -15,7 +15,7 @@ function Topbar () {
                 <a href="https://github.com/gracebodur" target='_blank' rel='noreferrer'>
                     <li className='social-list'><img className='social-icons' src={github} alt="github icon" /></li>
                 </a>
-                <a href="mgracebodur@gmail.com" target='_blank' rel='noreferrer'>
+                <a href="mailto:mgracebodur@gmail.com" target='_blank' rel='noreferrer'>
                     <li className='social-list'><img className='social-icons' src={mail} alt="mail icon" /></li>
                 </a>
                 <a href="https://docs.google.com/document/d/1gSfBTaGemXr8q5M3qIJ9E74bn7ojFcPs9axVP9qzbcM/edit?usp=sharing" target='_blank' rel='noreferrer'>
