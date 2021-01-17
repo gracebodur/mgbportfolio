b5c6e5fdfa2522af61ee404e62a60acca1d7efbf
Grace <mgracebodur@gmail.com>
2021-01-16 20:48:24 -0500

    fix links attributes

--- src/Topbar.js
+++ src/Topbar.js
@@ -5,24 +5,24 @@ import mail from './assets/email.svg'
 import docs from './assets//google-docs.svg'
 import './Topbar.css'
 
-function Topbar() {
+function Topbar () {
     return (
         <section className='topbar'>
-        <ul className='social'>
-				<a href="http://linkedin.com/in/mgracebodur">
-					<li className='social-list'><img className='social-icons' src={linkedin} alt="linkedin icon" target='_blank'/></li>
-				</a>
-				<a href="https://github.com/gracebodur">
-					<li className='social-list'><img className='social-icons' src={github} alt="github icon" target='_blank' /></li>
-				</a>
-				<a href="mgracebodur@gmail.com">
-					<li className='social-list'><img className='social-icons' src={mail} alt="mail icon" target='_blank' /></li>
-				</a>
-                <a href="https://docs.google.com/document/d/1gSfBTaGemXr8q5M3qIJ9E74bn7ojFcPs9axVP9qzbcM/edit?usp=sharing">
-                    <li className='social-list'><img className='social-icons' src={docs} alt="document icon" target='_blank' />
-					</li>
-				</a>
-        </ul>
+            <ul className='social'>
+                <a href="http://linkedin.com/in/mgracebodur" target='_blank' rel='noreferrer'>
+                    <li className='social-list'><img className='social-icons' src={linkedin} alt="linkedin icon" /></li>
+                </a>
+                <a href="https://github.com/gracebodur" target='_blank' rel='noreferrer'>
+                    <li className='social-list'><img className='social-icons' src={github} alt="github icon" /></li>
+                </a>
+                <a href="mgracebodur@gmail.com" target='_blank' rel='noreferrer'>
+                    <li className='social-list'><img className='social-icons' src={mail} alt="mail icon" /></li>
+                </a>
+                <a href="https://docs.google.com/document/d/1gSfBTaGemXr8q5M3qIJ9E74bn7ojFcPs9axVP9qzbcM/edit?usp=sharing" target='_blank' rel='noreferrer'>
+                    <li className='social-list'><img className='social-icons' src={docs} alt="document icon" />
+                    </li>
+                </a>
+            </ul>
         </section>
     )
 }
