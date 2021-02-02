8f6221587715738ff1190a799030bfba0e606c92
Grace <mgracebodur@gmail.com>
2021-02-01 23:43:15 -0500

    fix section title's padding and margins

--- src/Skills/Skills.js
+++ src/Skills/Skills.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Element } from 'react-scroll'
 import SkillsList from '../SkillsList/SkillsList'
 import skillsItems from '../skillsItems'
 import './Skills.css'
@@ -8,14 +7,14 @@ function Skills () {
     const skillsData = skillsItems.map( item => <SkillsList
         key={item.id} iconUrl={item.iconUrl} iconName={item.iconName} /> )
     return (
-        <Element id='skills' name='skills'>
-            <div className='skills-title text-center'>
-                <h1>Skills and Tools</h1>
+        <section id='skills'>
+            <div className='text-center'  >
+                <h1 className='skills-title'>Skills and Tools</h1>
             </div>
-            <div className='container text-center d-flex justify-content-around'>
+            <div className='container text-center d-flex justify-content-around skills-wrapper'>
                 {skillsData}
             </div>
-        </Element>
+        </section>
     )
 }
 
