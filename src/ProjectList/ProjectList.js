79b5d7152f78c2e5838e539333c67fd2312b33d8
Grace <mgracebodur@gmail.com>
2021-02-14 21:57:47 -0500

    change app screenshots

--- src/ProjectList/ProjectList.js
+++ src/ProjectList/ProjectList.js
@@ -15,8 +15,8 @@ function ProjectList ( props ) {
                                     <h3 className="title">{props.title}</h3>
                                     <span className="post">{props.description}</span>
                                     <ul className="icon">
-                                        <li><a href={props.codeLink}><i className="fas fa-code"></i></a></li>
-                                        <li><a href={props.liveLink}><i className="fas fa-link"></i></a></li>
+                                        <li><a href={props.codeLink} target='_blank' rel="noreferrer"><i className="fas fa-code" ></i></a></li>
+                                        <li><a href={props.liveLink} target='_blank' rel="noreferrer"><i className="fas fa-link" ></i></a></li>
                                     </ul>
                                 </div>
                             </div>
--- src/Skills/Skills.js
+++ src/Skills/Skills.js
@@ -3,19 +3,22 @@ import SkillsList from '../SkillsList/SkillsList'
 import skillsItems from '../skillsItems'
 import './Skills.css'
 
-function Skills () {
-    const skillsData = skillsItems.map( item => <SkillsList
-        key={item.id} iconUrl={item.iconUrl} iconName={item.iconName} /> )
-    return (
-        <section id='skills'>
-            <div className='text-center'  >
-                <h1 className='skills-title'>Skills and Tools</h1>
-            </div>
-            <div className='container text-center d-flex justify-content-around skills-wrapper'>
-                {skillsData}
-            </div>
-        </section>
-    )
+function Skills() {
+	const skillsData = skillsItems.map((item) => (
+		<SkillsList key={item.id} iconUrl={item.iconUrl} iconName={item.iconName} />
+	))
+	return (
+		<section id='skills'>
+			<div className='text-center'>
+				<h1 className='skills-title'>Skills and Tools</h1>
+			</div>
+			<div className='container text-center d-flex justify-content-around skills-wrapper'>
+				<div className='row'>
+                    {skillsData}
+                </div>
+			</div>
+		</section>
+	)
 }
 
 export default Skills
