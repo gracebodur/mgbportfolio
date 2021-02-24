78fd5d016e404457f374a4496dbabd7824ebf801
Grace <mgracebodur@gmail.com>
2021-02-24 13:20:36 -0500

    add responsive designs

--- src/Intro/Intro.js
+++ src/Intro/Intro.js
@@ -5,7 +5,7 @@ function Intro () {
     return (
         <section className='intro'>
             <h1 className='intro-title'>Frontend Developer</h1>
-            <h4 className='intro-text'>Hi there, prototype and code interfaces from Fuquay Varina, NC.</h4>
+            <h4 className='intro-text'>Hi there, I prototype and code interfaces</h4>
             <div className='intro-actions'>
                 <a href='#projects' className='intro-link'>
                     <svg width="29" height="29" xmlns="http://www.w3.org/2000/svg"><g transform="translate(1 1)" stroke="#D9B002" fill="none" fillRule="evenodd"><path strokeWidth="1.82" strokeLinecap="round" strokeLinejoin="round" d="M9.1 12.012l4.095 4.55 4.095-4.55"></path><circle strokeWidth="1.638" cx="13.195" cy="13.195" r="13.195"></circle></g></svg>
