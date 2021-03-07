9785ed0144b73f05a657e7e26f4c8d1de6cb57b8
Grace <mgracebodur@gmail.com>
2021-03-06 23:19:31 -0500

    changes: -add info details to about and intro -replace image with placeholder -create resume component

--- src/About/About.js
+++ src/About/About.js
@@ -1,15 +1,19 @@
 import React from 'react'
-import avatar from '../assets/avatar.webp'
+// import avatar from '../assets/avatar.webp'
 import './About.css'
 
 function About () {
     return (
         <div className='about-wrapper' id='about'>
             <section className="about-section">
-                <img className="grace-avatar" src={avatar} alt='Marie Grace Bodur profile pic' />
+                <img className="grace-avatar" src='https://placeholder.pics/svg/400x650' alt='Marie Grace Bodur profile pic' />
                 <div className="container-s">
-                    <h2 className="about-title">Working remotely, based in North Carolina</h2>
-                    <h5 className="about-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi magni maxime odit amet fugit esse. Blanditiis voluptatibus natus tenetur officiis. Autem tempora ducimus labore, velit est culpa natus similique amet consectetur error assumenda voluptas laudantium dolorum in nostrum minima alias nemo dicta, sed hic nisi. Dignissimos, nulla voluptatibus! Accusantium, impedit.</h5>
+                    <h2 className="about-title text-left">Working remotely, based in North Carolina</h2>
+                    <p className='about-text text-left'>Hello World!</p>
+                    <p className='about-text text-left'>I'm Grace Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, modi.</p>
+                    <p className="about-text text-left">I see myself as a full-stack web developer with a strength in the front-end and a passion for helping others, clean code and great UI/UX. I love being a developer because I can combine my creativity and analytical traits to break down complex problems and solve them efficiently. I believe that creating eloquently designed tools and applications can be an enormous driver in improving and growing an organization.I can work closely with other team members, but I work just as well independently. I work comfortably under pressure.</p>
+                    <p className='about-text text-left'>I enjoy traveling and learning about new cultures, new languages and new ways of thinking. It has made me an empathetic and open-minded individual, who can grasp new concepts quickly and adapt easily to changes in the work environment.</p>
+                    <p className='about-text text-left'>I'm excited to <a href="https://www.linkedin.com/in/mgracebodur/" className='lead connect' target='_blank' rel='noreferrer' alt='connect with linkedin'>connect</a> with companies and colleagues that inspire me!</p>
                 </div>
             </section>
         </div>
--- src/Intro/Intro.js
+++ src/Intro/Intro.js
@@ -5,9 +5,9 @@ function Intro () {
     return (
         <section className='intro'>
             <h1 className='intro-title'>Frontend Developer</h1>
-            <h4 className='intro-text'>Hi there, I prototype and code interfaces</h4>
+            <h4 className='intro-text'>Hi there, I develop fully responsive web applications, and write clean, high- quality code that is scalable and maintainable.</h4>
             <div className='intro-actions'>
-                <a href='#projects' className='intro-link'>
+                <a href='#projects' className='intro-link' alt='project link'>
                     <svg width="29" height="29" xmlns="http://www.w3.org/2000/svg"><g transform="translate(1 1)" stroke="#D9B002" fill="none" fillRule="evenodd"><path strokeWidth="1.82" strokeLinecap="round" strokeLinejoin="round" d="M9.1 12.012l4.095 4.55 4.095-4.55"></path><circle strokeWidth="1.638" cx="13.195" cy="13.195" r="13.195"></circle></g></svg>
                     <span className="link">See projects</span>
                </a>
