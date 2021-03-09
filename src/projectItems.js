2cacb60d0eb69c0db0328922f351ed5792c354b1
Grace <mgracebodur@gmail.com>
2021-03-09 02:27:57 -0500

    update each apps description

--- src/projectItems.js
+++ src/projectItems.js
@@ -8,7 +8,7 @@ const projectItems = [
         id: 1,
         imgUrl: gottalotto,
         title: 'GottaLotto',
-        description: "GottaLotto is a team project built to show our knowledge of Data Structure and Algorithms by creating a guessing game app. GottaLotto is for lotto enthusiasts to have plain old fun in guessing the winning numbers for each Saturday's PowerBall 's drawing. It allows users to submit their guesses for the upcoming drawing and a text area where users can include their lottery experience message. Submitted guesses are compared and scored based on how close they get from the Powerball winning numbers.",
+        description: "GottaLotto is for lotto enthusiasts to have plain old fun in guessing the winning numbers for each Saturday's PowerBall 's drawing. Collaborated with team members to plan website development, converting mockups into usable web presence with HTML, CSS, JavaScript, React.js, Node, PostgreSQL, and Cron Job coding.",
         liveLink: 'https://malekandgrace-gottalotto.now.sh/',
         codeLink: 'https://github.com/gracebodur/gottalotto-client'
     },
@@ -16,7 +16,7 @@ const projectItems = [
         id: 2,
         imgUrl: startsmall,
         title: 'Start Small',
-        description: "As a former teacher at a school that serves low-income communities, I experienced a lack of classroom supplies to help my students keep on learning. Start small is an independent project built to help fund classroom projects posted by students or teachers from across the country. The app shows classroom project listings that users can review based on 1 to 5 stars. It also provides an external link to the specific projects donating site.",
+        description: "Start small is an individual capstone project built to help fund classroom projects posted by students or teachers from across the country. The app shows classroom project listings that users can review based on 1 to 5 stars. It also provides an external link to the specific projects donating site. Built with HTML, CSS, JavaScript, React.js, Node.js, Express.js, PostgreSQL. Tested using Enzyme, Mocha and Chai testing libraries.",
         liveLink: 'https://start-small.now.sh/',
         codeLink: 'https://github.com/gracebodur/start-small'
     },
@@ -24,7 +24,7 @@ const projectItems = [
         id: 3,
         imgUrl: scrabblesolver,
         title: 'Scrabble Solver',
-        description: "Scrabble solver is a team project that is built and inspired by the popular word game Scrabble. The application produces valid words based on the provided scrabble letters or tiles. It shows dictionary definitions when clicking on valid word results and allows scrabble enthusiasts to sort word results by score or length.",
+        description: "Scrabble solver is a team project that is built and inspired by the popular word game Scrabble. The application produces valid words based on the provided scrabble letters or tiles. It shows users dictionary definitions when clicking on valid word results and allows scrabble enthusiasts to sort word results by score or length. Developed using HTML5, CSS3, Tailwind CSS, JavaScript, JQuery and Rest API.",
         liveLink: 'https://scrabble.codybarr.dev/',
         codeLink: 'https://github.com/codybarr-thinkful/scrabble-solver'
     },
@@ -32,7 +32,7 @@ const projectItems = [
         id: 4,
         imgUrl: jot,
         title: 'Jot',
-        description: "Jot is a note taking app where users can add, edit and delete a note. Notes are stored and access using users LocalStorage. A creative feature of Jot is that it enable users to change a notes background color by choosing 4 available colors from the tooltip. This project was built using JavaScript, HTML5 and CSS3.  ",
+        description: "A note taking app based on Google's Keep where users can add, edit and delete a note. Notes are stored and access using LocalStorage. Planned, designed, implemented, developed and converted mockups into usable web presence with HTML, CSS and JavaScript coding.",
         liveLink: 'https://gracebodur.github.io/jot/',
         codeLink: 'https://github.com/gracebodur/jot'
     }
