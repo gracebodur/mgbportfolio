dee87dd570b16a9bef37f9ddfa44bd05225e6976
Marie Grace Bodur <mgracebodur@gmail.com>
2021-07-27 22:02:06 -0400

    Changes:
    • add resume header logo and icons
    • uninstall boxicons dependency and use cdn intead

--- src/pages/Resume/ResumePage/ResumePage.jsx
+++ src/pages/Resume/ResumePage/ResumePage.jsx
@@ -4,7 +4,73 @@ import "./ResumePage.css";
 const ResumePage = () => {
   return (
     <>
-      <header className="l-header" id="r-header"></header>
+      <header className="l-header" id="r-header">
+        <nav className="r-nav bd-container">
+          <a href="https://www.mariegracebodur.com/" className="nav_logo">
+            Grace
+          </a>
+          <div className="nav__menu" id="nav-menu">
+            <ul className="nav__list">
+              <li className="nav__item">
+                <a
+                  href="https://www.mariegracebodur.com/"
+                  className="nav__link"
+                >
+                  <i className="bx bx-home"></i>Home
+                </a>
+              </li>
+              <li className="nav__item">
+                <a
+                  href="https://www.mariegracebodur.com/"
+                  className="nav__link"
+                >
+                  <i class="bx bx-user"></i>Profile
+                </a>
+              </li>
+              <li className="nav__item">
+                <a
+                  href="https://www.mariegracebodur.com/"
+                  className="nav__link"
+                >
+                  <i class="bx bx-code-block"></i>Projects
+                </a>
+              </li>
+              <li className="nav__item">
+                <a
+                  href="https://www.mariegracebodur.com/"
+                  className="nav__link"
+                >
+                  <i class="bx bx-book"></i>Education
+                </a>
+              </li>
+              <li className="nav__item">
+                <a
+                  href="https://www.mariegracebodur.com/"
+                  className="nav__link"
+                >
+                  <i class="bx bx-receipt"></i>Skills
+                </a>
+              </li>
+              <li className="nav__item">
+                <a
+                  href="https://www.mariegracebodur.com/"
+                  className="nav__link"
+                >
+                  <i class="bx bx-briefcase-alt"></i>Experience
+                </a>
+              </li>
+              <li className="nav__item">
+                <a
+                  href="https://www.mariegracebodur.com/"
+                  className="nav__link"
+                >
+                  <i class="bx bx-award"></i>Certificates
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </header>
       <main className="l-main bd-container">
         {/* <!-- All elements within this div, is generated in PDF --> */}
         <div className="resume" id="area-cv">
@@ -17,33 +83,45 @@ const ResumePage = () => {
             </section>
 
             {/* <!--========== SOCIAL ==========--> */}
-            <section className="social section"></section>
+            <section className="r-social r-section"></section>
 
             {/* <!--========== PROJECTS ==========--> */}
-            <section className="projects section" id="r-projects"></section>
+            <section className="r-projects r-section" id="r-projects"></section>
 
             {/* <!--========== EDUCATION ==========--> */}
-            <section className="education section" id="education"></section>
+            <section
+              className="r-education r-section"
+              id="r-education"
+            ></section>
 
             {/* <!--========== SKILLS  ==========--> */}
-            <section className="skills section" id="r-skills"></section>
+            <section className="r-skills r-section" id="r-skills"></section>
           </div>
 
           <div className="resume__right">
             {/* <!--========== EXPERIENCE ==========--> */}
-            <section className="experience section" id="experience"></section>
+            <section
+              className="r-experience r-section"
+              id="experience"
+            ></section>
 
             {/* <!--========== CERTIFICATES ==========--> */}
-            <section className="certificate section" id="certificates"></section>
+            <section
+              className="r-certificate r-section"
+              id="certificates"
+            ></section>
 
             {/* <!--========== REFERENCES ==========--> */}
-            <section className="references section" id="references"></section>
+            <section
+              className="r-references r-section"
+              id="references"
+            ></section>
 
             {/* <!--========== LANGUAGES ==========--> */}
-            <section className="languages section"></section>
+            <section className="r-languages r-section"></section>
 
             {/* <!--========== INTERESTS ==========--> */}
-            <section className="interests section"></section>
+            <section className="r-interests r-section"></section>
           </div>
         </div>
       </main>
